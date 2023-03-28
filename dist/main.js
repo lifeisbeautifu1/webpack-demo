8feb0c9c1cb1378c8ae7693f526c0b92f1153c9c
Potato <poltoradnevone@gmail.com>
2023-03-28 05:35:49 +0400

    asset-management

--- dist/main.js
+++
@@ -1,1 +0,0 @@
-(()=>{"use strict";console.log(5),document.body.appendChild((()=>{const e=document.createElement("div");return e.innerHTML=["Hello","webpack"].join(" "),e})())})();
--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import { sum } from "./sum";
+import "./style.css";
+import Avatar from "./avatar.jpeg";
 
 console.log(sum(2, 3));
 
 const component = () => {
   const element = document.createElement("div");
 
+  const image = new Image();
+  image.src = Avatar;
+
   element.innerHTML = ["Hello", "webpack"].join(" ");
+  element.classList.add("hello");
+  element.appendChild(image);
 
   return element;
 };
